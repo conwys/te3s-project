a9ebc073c4582c5ec2b1ac9bc9a88edb1bf82392
conwys <drkheartt@gmail.com>
2022-12-12 22:43:47 +0000

    fontawesome icons

--- te3s-project/src/components/Profile/ProfileComponents/Socials.js
+++ te3s-project/src/components/Profile/ProfileComponents/Socials.js
@@ -1,16 +1,17 @@
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 export default function Socials({ yt, st, ds }) {
     return (
         <div className="socials-container">
             <div className="socials-yt">
-                <a href={yt}>Hi</a>
+                <a href={yt}><FontAwesomeIcon icon={brands('youtube')}/></a>
             </div>
             <div className="socials-steam">
-                <a href={st}>Hi</a>
+                <a href={st}><FontAwesomeIcon icon={brands('steam-symbol')}/></a>
             </div>
             <div className="socials-discord">
-                <a href={ds}>Hi</a>
+                <a href={ds}><FontAwesomeIcon icon={brands('discord')}/></a>
             </div>
         </div>
     )
