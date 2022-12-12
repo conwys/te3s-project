4feb8e75ffc5ecc2a6b0b6ea6de4ea31bba8ae08
conwys <drkheartt@gmail.com>
2022-12-12 22:15:52 +0000

    started website

---
+++ te3s-project/src/components/Navbar/index.js
@@ -0,0 +1,24 @@
+import './styles.css';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <Link to="/" className="site-title">te3s</Link>
+            <ul>
+                <CustomLink to="/members">members</CustomLink>
+                <CustomLink to="/about">about</CustomLink>
+            </ul>
+        </nav>
+    )
+}
+
+function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to} {...props}>{children}</Link>
+        </li>
+    )
+}
