c4ca03fad5665f94b1781972bb5a0b10a9a9a5cb
conwys <drkheartt@gmail.com>
2022-12-14 23:12:41 +0000

    database changes

--- te3s-backend/models/index.js
+++ te3s-backend/models/index.js
@@ -1,12 +1,9 @@
 import { pool } from "../db/index.js";
 
 // async function to get by name
-async function getByName(name) {
+async function getMembers() {
   const results = await pool.query(
-    `SELECT * FROM cards
-    WHERE LOWER(CONCAT(first_name,' ', last_name)) LIKE LOWER('%'||$1||'%') 
-    ORDER BY last_name; `,
-    [name]
+    `SELECT * FROM cards`
   );
   const rows = results.rows;
   return rows;
--- te3s-backend/routes/index.js
+++ te3s-backend/routes/index.js
@@ -2,23 +2,16 @@ import express from "express";
 const router = express.Router();
 
 import {
-  getByName,
+  getMembers,
 } from "../models/index.js";
 
 // GET cards by name or email
-router.get("/", async (req, res) => {
-  if (req.query.name !== undefined) {
-    const result = await getByName(req.query.name);
-    res.json({
-      success: true,
-      payload: result,
-    });
-  } else if (req.query.email !== undefined) {
-    const result = await getByEmail(req.query.email);
-    res.json({
-      success: true,
-      payload: result,
-    });
+router.get("/", async (req, res) => {{
+  const result = await getMembers();
+  res.json({
+    success: true,
+    payload: result,
+  });
   }
 });
 
